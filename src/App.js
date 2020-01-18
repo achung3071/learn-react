bdc2e5919289f94a96e605b7af36caca71932c8b
Andrew Chung <drew.c3071@gmail.com>
2020-01-18 06:46:44 -0600

    update db with user shopping carts

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   };
 
   return (
-    <CartProvider>
+    <CartProvider user={user}>
       <InventoryProvider>
         {user ? (
           <div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,12 +1,31 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
+import firebase from "firebase/app";
+import "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDsLY-WXBtj9SVEbN2mHh3naElCz5a1W6s",
+  authDomain: "learn-react-5588c.firebaseapp.com",
+  databaseURL: "https://learn-react-5588c.firebaseio.com",
+  projectId: "learn-react-5588c",
+  storageBucket: "learn-react-5588c.appspot.com",
+  messagingSenderId: "1089750649744",
+  appId: "1:1089750649744:web:9e0e44d88621f47a174115"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database().ref();
 export const CartContext = createContext();
 
-const CartProvider = props => {
+const CartProvider = ({ user, children }) => {
   const [cartProducts, setCartProducts] = useState([]);
+  useEffect(() => {
+    if (user) {
+      db.child("carts").update({ [user.uid]: cartProducts });
+    }
+  });
   return (
     <CartContext.Provider value={{ cartProducts, setCartProducts }}>
-      {props.children}
+      {children}
     </CartContext.Provider>
   );
 };
